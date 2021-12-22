570ddea06021158dd27c350a4989f27e670b7754
Adel Hamad <adel.f.hamad@gmail.com>
2021-12-22 14:13:55 +0400

    awiait for update

--- pages/api/snippet.js
+++ pages/api/snippet.js
@@ -43,7 +43,7 @@ async function handler(req, res) {
       if (data?.data?.[0]) {
         const dataItem = data.data[0]
         const item = snippetObject(dataItem.attributes.password ? { attributes: { password: 'any' } } : dataItem)
-        if (item.id) axios.put(`${process.env.BACKEND_URL}/api/snippets/${item.id}`, { data: { views: parseInt(item.views) + 1 } })
+        if (item.id) await axios.put(`${process.env.BACKEND_URL}/api/snippets/${item.id}`, { data: { views: parseInt(item.views) + 1 } })
         statusCode = 200
         response = item
       } else {
